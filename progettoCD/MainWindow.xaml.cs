528068383a8010ddad19acfa9e41df1f194e6dc7
Francesco Moretti <FRANCESCO.MORETTI@local.ittcesena.it>
2022-11-05 09:22:20 +0100

    Versione 2.1 sviluppo del main

--- progettoCD/MainWindow.xaml.cs
+++ progettoCD/MainWindow.xaml.cs
@@ -24,5 +24,51 @@ namespace progettoCD
         {
             InitializeComponent();
         }
+        bool flag = true;
+        List<brano> listaBrani = new List<brano>();
+        cd disco; //un solo disco inizializzato quando clicco il bottone;
+
+        private void btnCreaBrano_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag)
+            {
+                MessageBox.Show("compila i campi di testo per titolo autore e durata");
+                flag = false;
+            }
+            else
+            {
+                brano br = new brano(txtTitolo.Text, txtAutore.Text, txtDurata.Text);
+                MessageBox.Show("titolo = " + br.getTitolo().ToString());
+                MessageBox.Show("autore = " + br.getAutore().ToString());
+                MessageBox.Show("durata = " + br.getDurata().ToString());
+                /*
+                br.setTitolo("titolo");
+                br.setAutore("autore");
+                br.setDurata("1212");
+                */
+                flag = true;   
+            }
+        }
+
+        private void btnCreaCD_Click(object sender, RoutedEventArgs e)
+        {
+            if (flag)
+            {
+                MessageBox.Show("compila i campi di testo per titolo e autore");
+                flag = false;
+            }
+            else
+            {
+                disco = new cd(txtTitolo.Text, txtAutore.Text);
+                MessageBox.Show("titolo = " + br.getTitolo().ToString());
+                MessageBox.Show("autore = " + br.getAutore().ToString());
+                MessageBox.Show("durata = " + br.getDurata().ToString());
+                /*
+                disco.setTitolo("titolo");
+                disco.setAutore("autore");
+                */
+                flag = true; 
+            }
+        }
     }
 }
